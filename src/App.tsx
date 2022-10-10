bc73843ca335174813d4bb5c1ccd78248c6d3a1b
James <jamesarnott2@gmail.com>
2022-10-10 17:09:23 +0100

    feat: added little animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,38 @@
 import React from 'react';
+import { XyzTransition, XyzTransitionGroup } from '@animxyz/react';
 
 import '@/index.css';
 
 const App = () => {
   const [count, setCount] = React.useState(0);
+  const [showCount, setShowCount] = React.useState(true);
+
+  const increment = () => {
+    setShowCount(false);
+    setTimeout(() => {
+      requestAnimationFrame(() => {
+        setShowCount(true);
+        setCount(count + 1);
+      });
+    }, 500);
+  };
 
   return (
     <div className="flex justify-center align-middle h-screen">
       <div className="bg-white m-auto p-10 rounded-xl w-1/2 text-center">
         <div className="underline text-5xl">Hello World</div>
         <div className="flex justify-center m-5">
-          <button className="text-2xl m-auto w-full bg-slate-200 hover:bg-slate-300 p-5 rounded-2xl" onClick={() => setCount(count + 1)}>
-            Click Count:
-            {count}
+          <button className="text-2xl m-auto w-full bg-slate-200 hover:bg-slate-300 p-5 rounded-2xl flex" onClick={() => increment()}>
+            <div className="flex-initial">
+              Click Count:
+            </div>
+            <XyzTransitionGroup xyz="fade down-100% back-2" duration={200} className="flex-1">
+              {showCount && (
+              <div>
+                {count}
+              </div>
+              )}
+            </XyzTransitionGroup>
           </button>
         </div>
       </div>
