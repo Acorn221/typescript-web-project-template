e12499b638321af7b5bc035c7840be18045fb334
James <jamesarnott2@gmail.com>
2022-10-10 16:51:27 +0100

    feat: created home page with click count

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,22 @@ import React from 'react';
 
 import '@/index.css';
 
-const App = () => (
-  <div>
-    <h1>Hello World</h1>
-  </div>
-);
+const App = () => {
+  const [count, setCount] = React.useState(0);
+
+  return (
+    <div className="flex justify-center align-middle h-screen">
+      <div className="bg-white m-auto p-10 rounded-xl w-1/2 text-center">
+        <div className="underline text-5xl">Hello World</div>
+        <div className="flex justify-center m-5">
+          <button className="text-2xl m-auto w-full bg-slate-200 hover:bg-slate-300 p-5 rounded-2xl" onClick={() => setCount(count + 1)}>
+            Click Count:
+            {count}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
